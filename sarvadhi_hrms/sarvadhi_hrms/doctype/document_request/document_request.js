b7af875544721df1de0654a2e670cc4f6493dec6
jeel-sarvadhi <jeel.r@sarvadhi.com>
2025-02-11 17:49:28 +0530

    Creating hrms

--- sarvadhi_hrms/sarvadhi_hrms/doctype/document_request/document_request.js
+++ sarvadhi_hrms/sarvadhi_hrms/doctype/document_request/document_request.js
@@ -1,8 +1,10 @@
 // Copyright (c) 2025, sarvadhi and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Document Request", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on('Document Request', {
+    onload: function(frm) {
+        if (!frm.doc.requested_by) {
+            frm.set_value('requested_by',frappe.session.user);
+        }
+    }
+});
